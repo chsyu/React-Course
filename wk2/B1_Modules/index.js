40e4ee1156d4fd50aafea196e3396004e1a8bf24
Chi-Shan Yu <chisamyu@seed.net.tw>
2021-03-01 20:13:34 +0800

    wk2 modules

---
+++ wk2/B1_Modules/index.js
@@ -0,0 +1,10 @@
+import { add, sub, mul, div } from "./math";
+import * as math from "./math";
+import greet from "./greet";
+greet('ntue');
+
+add(4, 2);
+sub(4, 2);
+mul(4, 2);
+div(4, 2);
+math.add(4, 2);
---
+++ wk2/B1_Modules/math.js
@@ -0,0 +1,6 @@
+const add = (arg1, arg2) => console.log(`The add result is ${arg1 + arg2}`);
+const sub = (arg1, arg2) => console.log(`The sub result is ${arg1 - arg2}`);
+const mul = (arg1, arg2) => console.log(`The mul result is ${arg1 * arg2}`);
+const div = (arg1, arg2) => console.log(`The div result is ${arg1 / arg2}`);
+
+export { add, sub, mul, div };
