539b7d7914e1cfed37f318deb9c723299a57d0c8
Chi-Shan Yu <chisamyu@seed.net.tw>
2021-03-10 16:49:50 +0800

    wk4 ant design project

---
+++ wk4/D1_HelloAntd/src/App.js
@@ -0,0 +1,11 @@
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import './App.css';
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ wk4/D1_HelloAntd/src/components/ProductList.js
@@ -0,0 +1,24 @@
+import { Row, Col } from "antd";
+
+import ProductItem from "./ProductItem";
+import products from "../json/products.json";
+
+export default function ProductList() {
+  return (
+    <Row gutter={[32, 32]}>
+    {products.map(product => (
+        <Col 
+          key={product.id} 
+          xs={{ span: 24 }} 
+          sm={{ span: 12 }} 
+          lg={{ span: 8 }}
+          xl={{ span: 6 }}
+          xxl={{ span: 4 }}
+        >
+          <ProductItem product={product}/>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
