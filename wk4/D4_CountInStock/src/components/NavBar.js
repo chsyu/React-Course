6096e2b86003c312bd85b88bbc495fff6c1a248d
Chi-Shan Yu <chisamyu@seed.net.tw>
2021-03-12 15:57:43 +0800

    add State

--- wk4/D4_CountInStock/src/components/NavBar.js
+++ wk4/D4_CountInStock/src/components/NavBar.js
@@ -1,26 +1,26 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
-    return (
-        <div className="nav-bar">
-            <Link to="/tableware" className="nav-item">
-                Tableware
-            </Link>
-            <Link to="/cookware" className="nav-item">
-                Cookware
-            </Link>
-            <Link to="/home-accessories" className="nav-item">
-                Home accessories
-            </Link>
-            <Link to="/lighting" className="nav-item">
-                Lighting
-            </Link>
-            <Link to="/textile" className="nav-item">
-                Textile
-            </Link>
-            <Link to="/furniture" className="nav-item">
-                Furniture
-            </Link>
-        </div>
-    );
+	return (
+		<div className="nav-bar">
+			<NavLink to="/tableware" className="nav-item" activeClassName="nav-item--active">
+				Tableware
+            </NavLink>
+			<NavLink to="/cookware" className="nav-item" activeClassName="nav-item--active">
+				Cookware
+            </NavLink>
+			<NavLink to="/home-accessories" className="nav-item" activeClassName="nav-item--active">
+				Home accessories
+            </NavLink>
+			<NavLink to="/lighting" className="nav-item" activeClassName="nav-item--active">
+				Lighting
+            </NavLink>
+			<NavLink to="/textile" className="nav-item" activeClassName="nav-item--active">
+				Textile
+            </NavLink>
+			<NavLink to="/furniture" className="nav-item" activeClassName="nav-item--active">
+				Furniture
+            </NavLink>
+		</div>
+	);
 }
