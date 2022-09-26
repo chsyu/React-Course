14ede4b76dfa3c8579299131b334e56209b544a1
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-09-27 07:40:40 +0800

    navbar component

--- wk5/E5_useDispatch_AddToCart/src/components/NavBar.js
+++ wk5/E5_useDispatch_AddToCart/src/components/NavBar.js
@@ -3,28 +3,28 @@ import { NavLink } from 'react-router-dom';
 export default function NavBar() {
     return (
         <div className="nav-bar">
-            <NavLink to="/category/tableware" 
-                className={["nav-item", (navData) => navData.isActive ? "active" : "" ]}>
+            <NavLink to="/category/tableware"
+                className={(navData) => (navData.isActive ? 'nav-item nav-item--active' : 'nav-item')}>
                 Tableware
             </NavLink>
-            <NavLink to="/category/cookware" 
-                className={["nav-item", (navData) => navData.isActive ? "nav-item--active" : "" ]}>
+            <NavLink to="/category/cookware"
+                className={(navData) => (navData.isActive ? 'nav-item nav-item--active' : 'nav-item')}>
                 Cookware
             </NavLink>
-            <NavLink to="/category/home-accessories" 
-                className={["nav-item", (navData) => navData.isActive ? "nav-item--active" : "" ]}>
+            <NavLink to="/category/home-accessories"
+                className={(navData) => (navData.isActive ? 'nav-item nav-item--active' : 'nav-item')}>
                 Home accessories
             </NavLink>
-            <NavLink to="/category/lighting" 
-                className={["nav-item", (navData) => navData.isActive ? "nav-item--active" : "" ]}>
+            <NavLink to="/category/lighting"
+                className={(navData) => (navData.isActive ? 'nav-item nav-item--active' : 'nav-item')}>
                 Lighting
             </NavLink>
-            <NavLink to="/category/textile" 
-                className={["nav-item", (navData) => navData.isActive ? "nav-item--active" : "" ]}>
+            <NavLink to="/category/textile"
+                className={(navData) => (navData.isActive ? 'nav-item nav-item--active' : 'nav-item')}>
                 Textile
             </NavLink>
-            <NavLink to="/category/furniture" 
-                className={["nav-item", (navData) => navData.isActive ? "nav-item--active" : "" ]}>
+            <NavLink to="/category/furniture"
+                className={(navData) => (navData.isActive ? 'nav-item nav-item--active' : 'nav-item')}>
                 Furniture
             </NavLink>
         </div>
