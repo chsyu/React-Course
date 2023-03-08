4c8528aadb8aabad4d17d3866ad14c610083f994
Chi-Shan Yu <chisamyu@seed.net.tw>
2023-03-08 09:37:26 +0800

    rename folder

--- wkA/Ja_AuthAPI/src/components/UserInfo/index.jsx
+++ wkA/Ja_AuthAPI/src/components/UserInfo/index.jsx
@@ -1,8 +1,13 @@
 import { UserSwitchOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 import styles from "./userinfo.module.css";
 
 export default function UserInfo(props) {
-   const goToProfile = () => { };
+   const navigate = useNavigate();
+   const goToProfile = () => {
+      navigate("/auth/login");
+   };
+
    return (
       <nav onClick={goToProfile} style={{ ...props.style }} className={styles.userInfo} >
          <UserSwitchOutlined className={styles.userInfoOutlined} />
